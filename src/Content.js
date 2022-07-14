f416a3d41d8800bbe3619440b3640f2e04c165f2
Rovshana <rovshanarzayeva2001@gmail.com>
2022-07-14 13:54:37 +0400

    importing images

--- src/Content.js
+++ src/Content.js
@@ -1,21 +1,20 @@
- 
+ import photo1 from "./components/Images/photo1.svg"
+ import photo2 from "./components/Images/photo2.svg"
+ import photo3 from "./components/Images/photo3.svg"
  export const content = [
   {
     id1: 1,
     title: "Grow Together",
     body: "Generate meaningful discussions with your audience and build a strong, loyal community. Think of the insightful conversations you miss out on with a feedback form.",
-  image: 'https://vibrant-ramanujan-fe049d.netlify.app/images/illustration-grow-together.svg'
-},
+  image: photo1},
 { 
    id2: 2,
    title: "Flowing Conversations",
    body: "You wouldn't paginate a conversation in real life, so why do it online? Our threads have just-in-time loading for a more natural flow.",
-image: "https://vibrant-ramanujan-fe049d.netlify.app/images/illustration-flowing-conversation.svg"
-},
+image: photo2},
 {
     id: 3,
     title: "Your Users",
     body: "It takes no time at all to integrate Huddle with your app's authentication solution. This means, once signed in to your app, your users can start chatting immediately.",
-image: "https://vibrant-ramanujan-fe049d.netlify.app/images/illustration-your-users.svg"
-}
+image: photo3}
 ];
